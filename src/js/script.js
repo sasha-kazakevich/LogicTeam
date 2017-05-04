2dfa4cd1733b76d3edd673508a05650425c70324
sasha-kazakevich <ssasshaka@gmail.com>
2017-05-04 18:14:42 +0300

    main-menu/fix all page

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 console.log('Ky');
+
+var map;
+
+    ymaps.ready(function(){
+
+    var placemark = new ymaps.Placemark([53.890128, 27.536903],{
+        hintContent: 'логистик'
+    },
+    {
+        preset: 'twirl#redDotIcon'
+    });
+
+    map = new ymaps.Map(document.getElementById('map'), {
+        center: [53.890128, 27.536903],
+        zoom: 16,
+        controls: [],
+        behaviors: ["scrollZoom"]
+    });
+    map.geoObjects.add(placemark);
+    map.behaviors.disable("scrollZoom");
+    // map.controls.add("zoomControl").add("typeSelector");
+});
