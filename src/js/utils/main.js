93dc4426c62801c7734c97d97112caa89c635be0
sasha-kazakevich <ssasshaka@gmail.com>
2017-05-12 03:31:12 +0300

    master/fix all services page

--- src/js/utils/main.js
+++ src/js/utils/main.js
@@ -98,6 +98,28 @@ var callback = {
           classToAdd: 'visible animated zoomIn',
           offset: 100
         });
+
+        var map;
+
+            ymaps.ready(function(){
+
+            var placemark = new ymaps.Placemark([53.890128, 27.536903],{
+                hintContent: 'логистик'
+            },
+            {
+                preset: 'twirl#redDotIcon'
+            });
+
+            map = new ymaps.Map(document.getElementById('map'), {
+                center: [53.890128, 27.536903],
+                zoom: 16,
+                controls: [],
+                behaviors: ["scrollZoom"]
+            });
+            map.geoObjects.add(placemark);
+            map.behaviors.disable("scrollZoom");
+            // map.controls.add("zoomControl").add("typeSelector");
+        });
     }
 }
 
